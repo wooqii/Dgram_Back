caa0f79340979c2fbd7ea5e74686412f0a134518
Dongwook <cpawookie@gmail.com>
2020-11-17 14:44:36 +0900

    Backcompleted
    Backend Completed

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
+import "./env";
+
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema"; 
+import "./passport";
+import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middleware";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
+const server = new GraphQLServer({ 
+  schema, 
+  context: ({ request }) => ({ request, isAuthenticated })
+});
 
-const resolvers = {
-  Query: {
-    hello: () => "Hi"
-  }
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
-  console.log(`Server running on  http://localhost:${PORT}`)
+  console.log(`👺 Server running on  http://localhost:${PORT}`)
 );
